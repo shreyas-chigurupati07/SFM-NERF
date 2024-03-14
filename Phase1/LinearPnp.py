55b65b696f890a30eb211c0f2d0794bdd0281c95
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2024-03-14 19:33:34 -0400

    Update LinearPnp.py

--- Phase1/LinearPnp.py
+++ Phase1/LinearPnp.py
@@ -5,6 +5,10 @@ RBE/CS Spring 2023: Classical and Deep Learning Approaches for
 Geometric Computer Vision
 Project 1: Buildings built in minutes - SfM and NeRF Phase 1 
 
+Author(s):
+Shreyas Chigurupati (schigurupati@wpi.edu)
+Worcester Polytechnic Institute
+"""
 
 
 # imports 
